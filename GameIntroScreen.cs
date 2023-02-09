f79842defca8061c846f56ecd0cccf0af34f2e4e
James Haig <jameshaig12@gmail.com>
2023-02-09 15:41:03 +0000

    redone most

---
+++ GameIntroScreen.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
+
+namespace AssignmentOneTextAdventure
+{
+    public class GameIntroScreen
+    {
+        //This whole section is made to auto resize the console window to full screen... yes its that bad
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+
+        public static void Main(string[] args)
+        {
+            //Resize console window
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            //Set colour of console to dark red and create ascii art for title
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+                        string menuArt = @"
+             ____  _                           _            ____                    
+            |  _ \| |__   __ _ _ __ __ _  ___ | |__  ___   / ___|   _ _ __ ___  ___ 
+            | |_) | '_ \ / _` | '__/ _` |/ _ \| '_ \/ __| | |  | | | | '__/ __|/ _ \
+            |  __/| | | | (_| | | | (_| | (_) | | | \__ \ | |__| |_| | |  \__ \  __/
+            |_|   |_| |_|\__,_|_|  \__,_|\___/|_| |_|___/  \____\__,_|_|  |___/\___|
+                                                                        
+                                                                             ";
+
+            Console.Write(menuArt);
+            Console.WriteLine("\nThe great pyramid of Prestonia, known to all as the final resting place of the mighty Pharoah Uclaneses III. " +
+                "\nMany fear this place, only few have dared to enter... but none have returned. Will you?");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nPress enter to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\nThree adventurers gather before the entrance of the pyramid. A Mage, A Warrior And A Rogue");
+            ClassSelection();
+        }
+
+        public static void ClassSelection()
+        {
+            //Switch statement within a while loop, allows for character selection
+            //While loop allows for validation, no valid answer =  go around again
+            bool continueGame = false;
+            
+            while(continueGame == false)
+            {
+                Console.WriteLine("\nYou Are?");
+                string classChoice = Console.ReadLine();
+                int playerClass;
+                switch (classChoice)
+                {
+                    case string a when a.Contains("Mage") || a.Contains("mage"):
+                        playerClass = 1;
+                        Console.WriteLine("\nYou, the Mage, approach the entrance first...");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("\nPress enter to continue...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        continueGame = true;
+                        GameIntro(playerClass);
+                        break;
+
+                    case string c when c.Contains("Warrior") || c.Contains("warrior"):
+                        playerClass = 2;
+                        Console.WriteLine("\nYou, the Warrior, approach the entrance first...");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("\nPress enter to continue...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        continueGame = true;
+                        GameIntro(playerClass);
+                        break;
+
+                    case string e when e.Contains("Rogue") || e.Contains("rogue"):
+                        playerClass = 3;
+                        Console.WriteLine("\nYou, the Rogue, approach the entrance first...");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("\nPress enter to continue...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        continueGame = true;
+                        GameIntro(playerClass);
+                        break;
+
+                    default:
+                        Console.WriteLine("It doesnt seem like that class is present right now");
+                        continueGame = false;
+                        break;
+                }
+            }
+        }
+
+        //This method is catching playerClass arg as param
+        public static void GameIntro(int playerClass)
+        {
+            //New ascii art and colour
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            string entranceArt = @"
+                                .,aadd""'    `""bbaa,.
+                            ,ad8888P'          `Y8888ba,
+                         ,a88888888              88888888a,
+                       a88888888888              88888888888a
+                     a8888888888888b,          ,d8888888888888a
+                    d8888888888888888b,_    _,d8888888888888888b
+                   d88888888888888888888888888888888888888888888b
+                  d8888888888888888888888888888888888888888888888b
+                 I888888888888888888888888888888888888888888888888I
+                ,88888888888888888888888888888888888888888888888888,
+                I8888888888888888PY8888888PY88888888888888888888888I
+                8888888888888888""  ""88888""  ""88888888888888888888888
+                8::::::::::::::'    `:::'    `:::::::::::::::::::::8
+                Ib:::::::::::""        ""        `::::::' `:::::::::dI
+                `8888888888P            Y88888888888P     Y88888888'
+                 Ib:::::::'              `:::::::::'       `:::::dI
+                  Yb::::""                  "":::::""           ""::dP
+                   Y88P                      Y8P               `P
+                    Y'                        ""
+                                                `:::::::::::;8""
+                       ""888888888888888888888888888888888888""
+                         `""8;::::::::::::::::::::::::::;8""'
+                            `""Ya;::::::::::::::::::;aP""'
+                                ``""""YYbbaaaaddPP""""''
+                                                                                 ";
+
+            Console.Write(entranceArt);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\nThe moon looms over the pyramids, you and your party stood at the base of the largest.");
+
+            //Thread.sleep used to space the time between lines
+            Thread.Sleep(1000);
+
+            //if else statement changes this sentence dependent on players class choice
+            if (playerClass == 1) 
+            {
+                Console.WriteLine("'I d-dont think i want to do this anymore' your warrior stammers, your rogue nodding in agreement");
+            }
+            else if (playerClass == 2)
+            {
+                Console.WriteLine("'I d-dont think i want to do this anymore' your mage stammers, your rogue nodding in agreement");
+            }
+            else if (playerClass ==3 )
+            {
+                Console.WriteLine("'I d-dont think i want to do this anymore' your rogue stammers, your mage nodding in agreement");
+            }
+
+            //Just dialog spaced by sleeps
+            Thread.Sleep(1000);
+            Console.WriteLine("'Fine.. i shall go alone if you two will be such cowards' you say, glancing sternly in their direction.");
+            Thread.Sleep(1000);
+            Console.WriteLine("You watch as your fellow party members scamper back off to saftey, shaking your head.");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nPress enter to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("As you approach the entrance you realise in the darkness its just a solid wall");
+            Thread.Sleep(1000);
+            Console.WriteLine("All those people who went missing had to get in somehow though, right?");
+
+            //Another switch in a while for choice validation
+            //NEEDS OPTIMISING WIP
+            bool continueGame = false;
+            
+            while(continueGame == false)
+            {
+                Console.WriteLine("\nWhat do you do?");
+                string playerEntranceChoiceOne = Console.ReadLine().ToLower();
+
+                switch (playerEntranceChoiceOne)
+                {
+                    case string a when a.Contains("Look") || a.Contains("look") || a.Contains("Clue") || a.Contains("clue") || a.Contains("Search") || a.Contains("search"):
+                        continueGame = true;
+                        RoomOne.EntrancePuzzle();
+                        break;
+
+                    default:
+                        Console.WriteLine("Hmm im not sure i can do that right now");
+                        break;
+                }
+            }              
+        }
+    }
+}
+
---
+++ RoomOne.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOneTextAdventure
+{
+    public class RoomOne
+    {
+        public static void EntrancePuzzle()
+        {
+                                string stonesArt = @"                                   _
+                                                     ,d8b,
+                                             _,,aadd8888888bbaa,,_
+                                        _,ad88P""""""8,  I8I  ,8""""""Y88ba,_
+                                     ,ad88P"" `Ya  `8, `8' ,8'  aP' ""Y88ba,
+                                   ,d8""' ""Yb   ""b, `b  8  d' ,d""   dP"" `""8b,
+                                  dP""Yb,  `Yb,  `8, 8  8  8 ,8'  ,dP'  ,dP""Yb
+                               ,ad8b, `Yb,  ""Ya  `b Y, 8 ,P d'  aP""  ,dP' ,d8ba,
+                              dP"" `Y8b, `Yb, `Yb, Y,`8 8 8',P ,dP' ,dP' ,d8P' ""Yb
+                             ,88888888Yb, `Yb,`Yb,`8 8 8 8 8',dP',dP' ,dY88888888,
+                             dP     `Yb`Yb, Yb,`8b 8 8 8 8 8 d8',dP ,dP'dP'     Yb
+                            ,8888888888b ""8, Yba888888888888888adP ,8"" d8888888888,
+                            dP        `Yb,`Y8P""""'             `""""Y8P',dP'        Yb
+                           ,88888888888P""Y8P'_.---.._     _..---._`Y8P""Y88888888888,
+                           dP         d'  8 '  ____  `. .'  ____  ` 8  `b         Yb
+                          ,888888888888   8   <(@@)>  | |  <(@@)>   8   888888888888,
+                          dP          8   8    `""""""         """"""'    8   8          Yb
+                         ,8888888888888,  8          ,   ,          8  ,8888888888888,
+                         dP           `b  8,        (.-_-.)        ,8  d'           Yb
+                        ,88888888888888Yaa8b      ,'       `,      d8aaP88888888888888,
+                        dP               """"8b     _,gd888bg,_     d8""""               Yb
+                       ,888888888888888888888b,    """"Y888P""""    ,d888888888888888888888,
+                       dP                   ""8""b,             ,d""8""                   Yb
+                      ,888888888888888888888888,""Ya,_,ggg,_,aP"",888888888888888888888888,
+                      dP                      ""8,  ""8""\x/""8""  ,8""                      Yb
+                     ,88888888888888888888888888b   8\\x//8   d88888888888888888888888888,
+                     8888bgg,_                  8   8\\x//8   8                  _,ggd8888
+                      `""Yb, """"8888888888888888888   Y\\x//P   8888888888888888888"""" ,dP""'
+                        _d8bg,_""8,              8   `b\x/d'   8              ,8""_,gd8b_
+                      ,iP""   ""Yb,8888888888888888    8\x/8    8888888888888888,dP""  `""Yi,
+                     ,P""    __,888              8    8\x/8    8              888,__    ""Y,
+                    ,8baaad8P"""":Y8888888888888888 aaa8\x/8aaa 8888888888888888P:""""Y8baaad8,
+                    dP""':::::::::8              8 8::8\x/8::8 8              8:::::::::`""Yb
+                    8::::::::::::8888888888888888 8::88888::8 8888888888888888::::::::::::8
+                    8::::::::::::8,             8 8:::::::::8 8             ,8::::::::::::8
+                    8::::::::::::8888888888888888 8:::::::::8 8888888888888888::::::::::::8
+                    8::::::::::::Ya             8 8:::::::::8 8             aP::::::::::::8
+                    8:::::::::::::888888888888888 8:::::::::8 888888888888888:::::::::::::8
+                    8:::::::::::::Ya            8 8:::::::::8 8            aP:::::::::::::8
+                    Ya:::::::::::::88888888888888 8:::::::::8 88888888888888:::::::::::::aP
+                    `8;:::::::::::::Ya,         8 8:::::::::8 8         ,aP:::::::::::::;8'
+                     Ya:::::::::::::::""Y888888888 8:::::::::8 888888888P"":::::::::::::::aP
+                     `8;::::::::::::::::::::""""""""Y8888888888888P""""""""::::::::::::::::::::;8'
+                      Ya:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::aP
+                       ""b;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;d""
+                        `Ya;:::::::::::::::::::::::::::::::::::::::::::::::::::::::;aP'
+                          `Ya;:::::::::::::::::::::::::::::::::::::::::::::::::::;aP'
+                             ""Ya;:::::::::::::::::::::::::::::::::::::::::::::;aP""
+                                ""Yba;;;:::::::::::::::::::::::::::::::::;;;adP""
+                                    `""""""""""""""Y888888888888888888888P""""""""""""""'
+                    ";
+
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Thread.Sleep(1000);
+            Console.WriteLine("\nYou light up a torch, blazing fire lighting up the stones before you...");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Thread.Sleep(2000);
+            Console.Write(stonesArt);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\nA large carving reveals itself in front of you, The mask of the Pharoah.");
+            Console.WriteLine("Beneath it you spy some barely legible words...");
+            Console.WriteLine("Walks on four in the morning... two in noon... three in the evening...");
+            Console.WriteLine("'Appears to be a riddle of some kind' you think to yourself");
+
+            bool continueGame = false;
+
+            while(continueGame == false)
+            {
+                Console.WriteLine("\nWhat is the answer of the riddle?");
+                string riddleAnswer = Console.ReadLine();
+
+                switch (riddleAnswer)
+                {
+                    case string a when a.Contains("Man") || a.Contains("man") || a.Contains("Person") || a.Contains("person") || a.Contains("Human") || a.Contains("human"):
+                        Console.WriteLine("\nThe eyes of the carving suddenly glow a brilliant red and you hear churning in the background");
+                        Console.WriteLine("Before your very eyes you see the solid wall slowly sink into the floor, revealing a pitch black hallway");
+                        continueGame = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Nothing Happened");
+                        continueGame = false;
+                        break;
+                }
+            }
+        }
+    }
+}
